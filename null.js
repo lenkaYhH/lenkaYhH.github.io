be5a457fa0fe7c6764de9b096ae29cbbe6afc03c
agilfor <alan.gil.forza@gmail.com>
2024-04-13 23:00:56 +0200

    make csp make more like original rijnmun website

--- null.js
+++ null.js
@@ -64,23 +64,23 @@ function panel() {
         bar3.style.setProperty('--i', '18')
     }
 }
-const face = document.getElementById("face");
-const pupils = document.querySelectorAll(".pupil");
+// const face = document.getElementById("face");
+// const pupils = document.querySelectorAll(".pupil");
 
-const move = (event) => {
-    let x = (event.clientX/(window.innerWidth/110));
-    let y = (event.clientY/7);
-    let fX = (event.clientX/20);
-    let fY = (event.clientY/20);
+// const move = (event) => {
+//     let x = (event.clientX/(window.innerWidth/110));
+//     let y = (event.clientY/7);
+//     let fX = (event.clientX/20);
+//     let fY = (event.clientY/20);
     
-    face.style.transform = `translate(${fX}px, ${fY}px)`;
+//     face.style.transform = `translate(${fX}px, ${fY}px)`;
     
-    // document.body.style.backgroundColor = `rgba(${y}, ${x}, ${fY}, 50%)`;
+//     // document.body.style.backgroundColor = `rgba(${y}, ${x}, ${fY}, 50%)`;
     
-    for (const pupil of pupils) {
-        pupil.style.transform = `translate(${x}px, ${y}px)`;
-    }
-}
+//     for (const pupil of pupils) {
+//         pupil.style.transform = `translate(${x}px, ${y}px)`;
+//     }
+// }
                         
 window.addEventListener('mousemove', move);
     function process() {
@@ -188,7 +188,7 @@ function show_content() {
     document.getElementsByClassName("cover")[0].style.display = "none";
 }
 function hide_content() {
-    document.getElementById('title').innerHTML = "Error 404: Page Not Found";
+    document.getElementById('title').innerHTML = "Board of Directors";
     document.getElementsByClassName("real-content")[0].style.display = "none";
     document.getElementsByClassName("cover")[0].style.display = "block";
 }
